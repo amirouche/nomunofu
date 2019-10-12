8054f09e18df4727cc045b52247abb3628683886
Amirouche <amirouche+dev@hyper.dev>
2019-10-12 10:48:35 +0200

    typofix

--- src/arew/scheme/cxr.scm
+++ src/arew/scheme/cxr.scm
@@ -2,7 +2,7 @@
 ;; SPDX-License-Identifier: CC0-1.0
 #!r6rs
 
-(library (scheme cxr)
+(library (arew scheme cxr)
   (export
     caaaar caaadr caaar caadar caaddr caadr cadaar cadadr cadar
     caddar cadddr caddr cdaaar cdaadr cdaar cdadar cdaddr cdadr
