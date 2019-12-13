77c05ad6d18264da576f93de11c6145f17b20f1a
Amirouche <amirouche+dev@hyper.dev>
2019-12-13 20:33:48 +0100

    nomunofu/index.scm: fixes
    - Import iri as scheme symbol: This is will make things worse for
      Python, but better for scheme and match the RDF specification.
    
    - Support @LANG, with an internal iri as an indirection that is
      nomunofu/lang/LANG.
    
    - Fix signature of ustore-gensym.

--- nomunofu/index.scm
+++ nomunofu/index.scm
@@ -10,16 +10,69 @@
 (import (nomunofu okvs ustore))
 
 
-(define (add/transaction transaction nstore ustore items)
-  (nstore-add! transaction
-               nstore
-               (map (lambda (item) (object->ulid transaction ustore item))
-                    items)))
+(define (string->triples transaction ustore string)
+  (let ((chars (string->list string)))
+    (call-with-values (lambda () (turtle-parse-item chars))
+      (lambda (subject rest)
+        (if (not subject)
+            #f
+            (call-with-values (lambda () (turtle-parse-item rest))
+              (lambda (predicate rest)
+                (call-with-values (lambda () (turtle-parse-item rest))
+                  (lambda (object rest)
+                    (cond
+                     ((not object) #f)
+                     ((char=? (car rest) #\^)
+                      (let ((type (list->string
+                                   (take-while (compose not char-whitespace?) rest))))
+                        (cond
+                         ((string=? type "^^<http://www.w3.org/2001/XMLSchema#dateTime>")
+                          (list (list subject predicate (turtle-parse-datetime object))))
+                         ((string=? type "^^<http://www.w3.org/2001/XMLSchema#integer>")
+                          (list (list subject predicate (string->number object))))
+                         ((string=? type "^^<http://www.w3.org/2001/XMLSchema#decimal>")
+                          ;; TODO: add support in okvs/pack
+                          (let ((out (string->number object)))
+                            (if (exact? out)
+                                (list (list subject predicate out))
+                                #f)))
+                         ((string=? type "^^<http://www.opengis.net/ont/geosparql#wktLiteral>")
+                          ;; TODO: support Point(5.4726111111111 49.497111111111)
+                          #f)
+                         ((string=? type "^^<http://www.w3.org/2001/XMLSchema#double>")
+                          ;; TODO: add support in okvs/pack
+                          (let ((out (string->number object)))
+                            (if (exact? out)
+                                (list (list subject predicate out))
+                                #f)))
+                         (else (raise type)))))
+                     ((char=? (car rest) #\@)
+                      (let ((lang (list->string (take (drop rest 1) 2)))
+                            (sym (ustore-gensym transaction ustore)))
+                        (list
+                         (list subject predicate sym)
+                         (list sym
+                               (string->symbol (string-append "nomunofu/lang/" lang))
+                               object))))
+                     ((and subject predicate object)
+                      (list (list subject predicate object)))
+                     (else (raise (list subject predicate object)))))))))))))
 
-(define (add app items)
+
+(define (add/transaction transaction nstore ustore line)
+  (let ((triples (string->triples transaction ustore line)))
+     (when triples
+       (for-each (lambda (triple)
+                   (nstore-add! transaction
+                                nstore
+                                (map (lambda (item) (object->ulid transaction ustore item))
+                                     triple)))
+                 triples))))
+
+(define (add app line)
   (engine-in-transaction (app-engine app) (app-okvs app)
     (lambda (transaction)
-      (add/transaction transaction (app-nstore app) (app-ustore app) items))))
+      (add/transaction transaction (app-nstore app) (app-ustore app) line))))
 
 (define (decode chars)
   (let loop ((chars chars)
@@ -44,7 +97,7 @@
 (define (turtle-parse-iri chars)
   (call-with-values (lambda () (span (lambda (x) (not (char=? x #\>))) chars))
     (lambda (item rest)
-      (values (list->string item) (cdr rest)))))
+      (values (string->symbol (list->string item)) (cdr rest)))))
 
 (define (turtle-parse-number chars)
   (call-with-values (lambda () (span char-numeric? chars))
@@ -70,58 +123,14 @@
                             ;; like: -34000-01-01T00:00:00Z
     (car (mktime (car (strptime "%FT%T" object)) "UTC"))))
 
-(define (turtle->scheme string)
-  (let ((chars (string->list string)))
-    (call-with-values (lambda () (turtle-parse-item chars))
-      (lambda (subject rest)
-        (if (not subject)
-            #f
-            (call-with-values (lambda () (turtle-parse-item rest))
-              (lambda (predicate rest)
-                (call-with-values (lambda () (turtle-parse-item rest))
-                  (lambda (object rest)
-                    (cond
-                     ((not object) #f)
-                     ((char=? (car rest) #\^)
-                      (let ((type (list->string (take-while (compose not char-whitespace?) rest))))
-                        (cond
-                         ((string=? type "^^<http://www.w3.org/2001/XMLSchema#dateTime>")
-                          (list subject predicate (turtle-parse-datetime object)))
-                         ((string=? type "^^<http://www.w3.org/2001/XMLSchema#integer>")
-                          (list subject predicate (string->number object)))
-                         ((string=? type "^^<http://www.w3.org/2001/XMLSchema#decimal>")
-                          ;; TODO: add support in okvs/pack
-                          (let ((out (string->number object)))
-                            (if (exact? out)
-                                (list subject predicate out)
-                                #f)))
-                         ((string=? type "^^<http://www.opengis.net/ont/geosparql#wktLiteral>")
-                          ;; TODO: support Point(5.4726111111111 49.497111111111)
-                          #f)
-                         ((string=? type "^^<http://www.w3.org/2001/XMLSchema#double>")
-                          ;; TODO: add support in okvs/pack
-                          (let ((out (string->number object)))
-                            (if (exact? out)
-                                (list subject predicate out)
-                                #f)))
-                         (else (raise type)))))
-                     ((char=? (car rest) #\@)
-                      (if (string=? (list->string (take rest 3)) "@en")
-                          (list subject predicate object)
-                          #f))
-                     ((and subject predicate object) (list subject predicate object))
-                     (else (raise (list subject predicate object)))))))))))))
-
 (define-public (subcommand-index app filename)
   (call-with-input-file filename
     (lambda (port)
       (let loop ((line (read-line port))
                  (index 0))
         (unless (eof-object? line)
-          (let ((items (turtle->scheme line)))
-            (when (= (modulo index 10000) 0)
-              (display index) (newline))
-            (when items
-              (add app items))
-            (loop (read-line port)
-                  (+ index 1))))))))
+          (when (= (modulo index 10000) 0)
+            (display index) (newline))
+          (add app line)
+          (loop (read-line port)
+                (+ index 1)))))))
